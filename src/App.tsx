3c3c716e8bf5f7d1d031b1f626cc847aaf21f0f1
Svetlana <svetlanka.ender@yandex.ru>
2023-03-04 12:45:18 +0200

    Fix StyledApp

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
     if (isLoading) return null;
 
     return (
-        <StyledApp
+        <StyledApp>
             {connected?
                 <BrowserRouter>
                     <Routes>
