399aec3adab9c4c1511ebe50b505494d30de9bcd
Aleksandr Khaustov <aleksandr.khaustov@superdao.co>
2023-03-17 10:54:17 +0800

    compute distance btw user and order locations

--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -2,23 +2,25 @@ import { useState } from "react"
 import { GoogleMap, LoadScript } from "@react-google-maps/api"
 import CustomMarker from "./CustomMarker"
 import { SelectedOrderBox } from "./SelectedOrderBox"
-import {OrderUser} from "../../models/order-user";
-import {useStore} from "@nanostores/react";
-import {locationData} from "../../store/Location";
+import { OrderUser } from "../../models/order-user"
+import { useStore } from "@nanostores/react"
+import { locationData } from "../../store/Location"
 
 const containerStyle = {
   display: "flex",
   flex: 1,
 }
+const libraries = ["geometry"] as ["geometry"]
 
 export function MapComponent(props: { ordersUsers: OrderUser[] }) {
   const [selectedOrder, selectOrder] = useState<string>()
   let { ordersUsers } = props
-    const location = useStore(locationData)
-    console.log("MapComponent")
-    console.log(ordersUsers)
+  const location = useStore(locationData)
+  console.log("MapComponent")
+  console.log(ordersUsers)
+
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_API}>
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_API} libraries={libraries}>
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={{
@@ -27,12 +29,12 @@ export function MapComponent(props: { ordersUsers: OrderUser[] }) {
         }}
         zoom={10}
         onClick={() => {
-            selectOrder(undefined)
-            console.log(selectedOrder)
+          selectOrder(undefined)
+          console.log(selectedOrder)
         }}
         options={{
           disableDefaultUI: true,
-          clickableIcons: false
+          clickableIcons: false,
         }}
       >
         {ordersUsers.map((orderUser, i) => (
@@ -45,7 +47,12 @@ export function MapComponent(props: { ordersUsers: OrderUser[] }) {
             orderId={orderUser.order.id as string}
           />
         ))}
-        {selectedOrder && <SelectedOrderBox orderUser={ordersUsers.find(o => o.order.id === selectedOrder)} />}
+        {selectedOrder && (
+          <SelectedOrderBox
+            orderUser={ordersUsers.find(o => o.order.id === selectedOrder)}
+            userLocation={{ lat: location.x, lng: location.y }}
+          />
+        )}
       </GoogleMap>
     </LoadScript>
   )
--- src/components/map/SelectedOrderBox.tsx
+++ src/components/map/SelectedOrderBox.tsx
@@ -1,15 +1,18 @@
 import { motion } from "framer-motion"
 import { OverlayView } from "@react-google-maps/api"
 import classes from "./Map.module.css"
-import {Link} from "react-router-dom";
-import {PATH_CHECKOUT} from "../../config/routes-config";
-import {OrderUser} from "../../models/order-user";
-import {useEffect, useState} from "react";
-import getAvatar from "../../utils/getAvatar";
-import {Button} from "../styled/styled";
+import { Link } from "react-router-dom"
+import { PATH_CHECKOUT } from "../../config/routes-config"
+import { OrderUser } from "../../models/order-user"
+import { useEffect, useMemo, useState } from "react"
+import getAvatar from "../../utils/getAvatar"
+import { Button } from "../styled/styled"
+import { useComputeDistance } from "./useComputeDistance"
+import { convertMetersToKilometers } from "../../utils/formatMeters"
 
 interface CustomMarkerProps {
   orderUser?: OrderUser
+  userLocation: google.maps.LatLngLiteral
 }
 
 const getPixelPositionOffset = (width: number, height: number) => ({
@@ -17,21 +20,25 @@ const getPixelPositionOffset = (width: number, height: number) => ({
   y: -(height / 2),
 })
 
-export function SelectedOrderBox({ orderUser }: CustomMarkerProps) {
+export function SelectedOrderBox({ orderUser, userLocation }: CustomMarkerProps) {
   if (!orderUser) return null
   const [avatarUrl, setAvatarUrl] = useState("")
-  useEffect(()=>{
-    getAvatar(Number(orderUser.person.id))
-        .then(res=>setAvatarUrl(res))
+  useEffect(() => {
+    getAvatar(Number(orderUser.person.id)).then(res => setAvatarUrl(res))
   }, [])
   console.log("orderUser")
   console.log(orderUser)
+
+  const orderLocation = {
+    lat: orderUser.order.location.x,
+    lng: orderUser.order.location.y,
+  }
+
+  const distance = useComputeDistance(orderLocation, userLocation)
+
   return (
     <OverlayView
-      position={{
-        lat: orderUser.order.location.x,
-        lng: orderUser.order.location.y,
-      }}
+      position={orderLocation}
       mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
       getPixelPositionOffset={getPixelPositionOffset}
     >
@@ -45,14 +52,12 @@ export function SelectedOrderBox({ orderUser }: CustomMarkerProps) {
         }}
       >
         <div className={classes.userCard}>
-          <img
-            src={avatarUrl}
-            className={classes.userAvatar}
-          />
+          <img src={avatarUrl} className={classes.userAvatar} />
           <div className={classes.userCardRight}>
             <div className={classes.userSlug}>@{orderUser.person.username}</div>
             <div className={classes.userInfo}>
-              2 km away, сompletion: <span className={classes.userSuccessPercent}>97%</span>
+              {distance ? `${convertMetersToKilometers(distance)} away` : "Distance unknown"}, сompletion:{" "}
+              <span className={classes.userSuccessPercent}>97%</span>
             </div>
           </div>
         </div>
@@ -69,7 +74,9 @@ export function SelectedOrderBox({ orderUser }: CustomMarkerProps) {
             </div>
           </div>
         </div>
-        <Link to={PATH_CHECKOUT} state={{order: orderUser.order, person: orderUser.person}}>Buy TON</Link>
+        <Link to={PATH_CHECKOUT} state={{ order: orderUser.order, person: orderUser.person }}>
+          Buy TON
+        </Link>
       </motion.div>
     </OverlayView>
   )
