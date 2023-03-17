257331616b99ccd6b9306168b083b08e9e471f71
Svetlana <svetlanka.ender@yandex.ru>
2023-03-17 11:42:51 +0200

    create design order confirmation

--- src/components/pages/order/OrderConfirmation.tsx
+++ src/components/pages/order/OrderConfirmation.tsx
@@ -1,67 +1,83 @@
-import {Order, OrderType} from "../../../models/order";
-import User from "../../../models/user";
-import React, {useState} from "react";
-import classes from "../find-orders/FindOrders.module.css";
-import {PATH_PROFILE} from "../../../config/routes-config";
-import {Link, useLocation, useNavigate} from "react-router-dom";
-import {Button} from "../../styled/styled";
-import {OrderUser} from "../../../models/order-user";
-import {Deal} from "../../../models/deal";
-import {dealsService, ordersService} from "../../../config/service-config";
-import {useStore} from "@nanostores/react";
-import {userData} from "../../../store/UserData";
+import { Order, OrderType } from "../../../models/order"
+import User from "../../../models/user"
+import React, { useState } from "react"
+import classes from "./OrderConfirmation.module.css"
+import { PATH_PROFILE } from "../../../config/routes-config"
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { Button } from "../../styled/styled"
+import { OrderUser } from "../../../models/order-user"
+import { Deal } from "../../../models/deal"
+import ButtonBack from "../../buttonBack/ButtonBack"
+import { dealsService, ordersService } from "../../../config/service-config"
+import { useStore } from "@nanostores/react"
+import { userData } from "../../../store/UserData"
 
 type CheckoutProps = {
-    order: Order,
-    orderOwner: User
+  order: Order
+  orderOwner: User
 }
 
-const OrderConfimation = ()=> {
+const OrderConfimation = () => {
+  const location = useLocation()
+  const deal: Deal = location.state
 
-    const location = useLocation();
-    const deal : Deal = location.state.deal;
-    const order : Order = location.state.order;
-    const navigate = useNavigate()
-    const user = useStore(userData)
+  const order: Order = location.state.order
+  const navigate = useNavigate()
+  const user = useStore(userData)
 
-    async function dealRequest(ownerId: string, deal: Deal) {
-        // TODO
-        // ordersService.addOrder()
-        const res = await dealsService.offerDeal(ownerId, deal)
-        console.log(res)
-    }
+  async function dealRequest(ownerId: string, deal: Deal) {
+    // TODO
+    // ordersService.addOrder()
+    const res = await dealsService.offerDeal(ownerId, deal)
+    console.log(res)
+  }
 
-    function showPopup(){
-        // TODO
-    }
+  function showPopup() {
+    // TODO
+  }
 
+  // TODO дописать логику кнопки конфирм
 
-    return (
-        <div>
-            <Link className={classes.backButton} to={PATH_PROFILE}>
-                go back
-            </Link>
-            <div>
-                <h1 className={classes.ordersTitle}>Order confirmation</h1>
-            </div>
+  const handleConfirm = async () => {
+    await dealRequest(user.id.toString(), deal)
+    showPopup()
+    navigate(PATH_PROFILE)
+  }
 
-            <div>
-                <div>
-                    <div>Amount {deal.amount} TON</div>
-                    <div>Amount {deal.amount*order.price} {order.currency}</div>
-                </div>
-            </div>
-            <div>
-                <Button>Cancel</Button>
-                <Button onClick={async ()=>{
-                    await dealRequest(user.id.toString(), deal)
-                    showPopup()
-                    navigate(PATH_PROFILE)
-                }}>Confirm</Button>
-            </div>
+  return (
+    <div className={classes.orderConfimationPage}>
+      <div>
+        <div className={classes.orderHeaders}>
+          <ButtonBack />
+          <h1 className={classes.orderTitle}>Order confirmation</h1>
         </div>
-    )
 
+        {/* TODO добавить значение комисии */}
+
+        <div className={classes.infoContainer}>
+          <div className={classes.infoItem}>
+            <p>Amount:</p>
+            <p>{deal.amount} TON</p>
+          </div>
+          <div className={classes.infoItem}>
+            <p>Amount::</p>
+            <p>
+              {deal.amount * order.price} {order.currency}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.buttonContainer}>
+        <button className={classes.buttonValueCancel} onClick={() => navigate(-1)}>
+          Cancel
+        </button>
+        <button className={classes.buttonValueCame} onClick={handleConfirm}>
+          Сonfirm
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default OrderConfimation
